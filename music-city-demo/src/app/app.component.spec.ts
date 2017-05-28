1894e0f90bdfca0ad8404565c92414573e2843e2
Jeremy Likness <jeremy@jeremylikness.com>
2017-05-28 14:00:29 -0400

    Fix tests

--- music-city-demo/src/app/app.component.spec.ts
+++ music-city-demo/src/app/app.component.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { AppSettingsService } from './app-settings.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [
         AppComponent
-      ],
+      ], 
+      providers: [AppSettingsService],
     }).compileComponents();
   }));
 
--- music-city-demo/src/app/bifurcation.service.spec.ts
+++ music-city-demo/src/app/bifurcation.service.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BifurcationService } from './bifurcation.service';
+import { AppSettingsService } from './app-settings.service';
 
 describe('BifurcationService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BifurcationService]
+      providers: [BifurcationService, AppSettingsService]
     });
   });
 
